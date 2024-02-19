450570519a71489612e534e9eef5dc29158a3cb2
Richiisontheway <mineiriccardo@gmail.com>
2024-02-19 18:53:41 +0100

    update

--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,12 @@
 <?php
-    require_once __DIR__.'./prodotti/Product.php';
-    //require_once __DIR__ . './prodotti/DogProduct.php';
-    //require_once __DIR__ . './prodotti/CatProduct.php';
-    require_once __DIR__ . './prodotti/FoodDog.php';
-    require_once __DIR__ . './prodotti/FoodCat.php';
+require_once __DIR__.'./prodotti/Product.php';
+//require_once __DIR__ . './prodotti/DogProduct.php';
+//require_once __DIR__ . './prodotti/CatProduct.php';
+require_once __DIR__ . './prodotti/FoodDog.php';
+require_once __DIR__ . './prodotti/FoodCat.php';
 
 
-    $product_one = new CatProduct(2,'collare',12.97,'cibo');
-    
-    echo $product_one->get_info();
+$product_one = new CatProduct(2,'collare',12.97,'cibo');
+
+echo $product_one->get_info();
     
